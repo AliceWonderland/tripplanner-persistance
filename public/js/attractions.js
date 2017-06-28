008124e75f5a065f348f6b11423c758bb1075350
Alice Chuang <smillaraaq@Alices-MacBook-Pro.local>
2017-06-28 18:09:01 -0400

    added in ajax to attractions.js

--- public/js/attractions.js
+++ public/js/attractions.js
@@ -7,15 +7,65 @@
  * to add an attraction in the `options` module.
  */
 
+
 var attractionsModule = (function () {
+    var enhanced={};
+
+    // HOTELS
+    $.ajax({
+        method: 'get',
+        url: '/api/hotels'
+    })
+        .then(function (responseData) {
+            console.log('data is received! ', responseData);
+            hotels=responseData;
+            // hotels.forEach(makeOption, $hotelSelect);
+            enhanced['hotels'] = hotels.map(attractionModule.create);
+        })
+        .catch(function (errorObj) {
+            console.log("Something wrong with Hotels AJAX");
+        });
+
+
+// RESTAURANTS
+    $.ajax({
+        method: 'get',
+        url: '/api/restaurants'
+    })
+        .then(function (responseData) {
+            console.log('data is received! ', responseData);
+            restaurants=responseData;
+            // restaurants.forEach(makeOption, $restaurantSelect);
+            enhanced['restaurants'] = restaurants.map(attractionModule.create);
+        })
+        .catch(function (errorObj) {
+            console.log("Something wrong with Restaurants AJAX");
+        });
+
+// ACTIVITIES
+    $.ajax({
+        method: 'get',
+        url: '/api/activities'
+    })
+        .then(function (responseData) {
+            console.log('data is received! ', responseData);
+            activities=responseData;
+            // activities.forEach(makeOption, $activitySelect);
+            enhanced['activities'] = activities.map(attractionModule.create);
+        })
+        .catch(function (errorObj) {
+            console.log("Something wrong with Activities AJAX");
+        });
+
+
 
   // application state
 
-  var enhanced = {
-    hotels: hotels.map(attractionModule.create),
-    restaurants: restaurants.map(attractionModule.create),
-    activities: activities.map(attractionModule.create),
-  };
+  // var enhanced = {
+  //   hotels: hotels.map(attractionModule.create),
+  //   restaurants: restaurants.map(attractionModule.create),
+  //   activities: activities.map(attractionModule.create),
+  // };
 
   // private helper methods (only available inside the module)
 
