a3d80dc04c5246f2435f3bb3d8be6fb5e49c53f2
Alice Chuang <smillaraaq@Alices-MacBook-Pro.local>
2017-06-28 16:32:26 -0400

    Day model

--- models/index.js
+++ models/index.js
@@ -5,9 +5,9 @@ var Hotel = require('./hotel');
 var Restaurant = require('./restaurant');
 var Activity = require('./activity');
 
-Hotel.belongsTo(Place);
-Restaurant.belongsTo(Place);
-Activity.belongsTo(Place);
+Day.belongsTo(Hotel);
+Day.belongsToMany(Restaurants,{through:day_restaurant});
+Day.belongsToMany(Activity,{through:day_activity});
 
 module.exports = {
 	db,
